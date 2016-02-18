98a3832b80ca6699b181e5547c13821622958016
nitingera1996 <nitin.gera.ece14@iitbhu.ac.in>
2016-02-18 19:37:13 +0530

    initial commit with an example

---
+++ apis/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+import requests
+import json
+from bs4 import BeautifulSoup
+from django.http import HttpResponse
+
+def openvpn_password(request):
+    response_dict={}
+    r  = requests.get("http://www.vpnbook.com/freevpn")
+    data = r.text
+    soup = BeautifulSoup(data, "html.parser")
+    tags=soup.find_all('li')
+    for t in tags:
+        l=str(t.get_text())
+        if l.startswith("Password"):
+            now_password=l.split(" ",1)[-1]
+            response_dict['password']=now_password
+    return HttpResponse(json.dumps(response_dict), content_type='application/javascript') 
+
