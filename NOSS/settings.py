74b9c5789a89fb4e1452807ae2c47e01632df27c
sanskar-modi <sanskar.modi.civ14@iitbhu.ac.in>
2016-02-20 06:43:06 +0530

    Bug fixed

--- NOSS/settings.py
+++ NOSS/settings.py
@@ -98,9 +98,9 @@ DATABASES = {
 }
 
 # Update database configuration with $DATABASE_URL.
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 #django-allauth configration
 
