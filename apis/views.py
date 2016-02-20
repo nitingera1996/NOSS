14ee923b2e9cc51ec1c1b09884a1b38554b8b26a
nitingera1996 <nitin.gera.ece14@iitbhu.ac.in>
2016-02-20 11:01:18 +0530

    Adding some keys and minor bug fix

--- apis/views.py
+++ apis/views.py
@@ -5,15 +5,18 @@ from bs4 import BeautifulSoup
 from django.http import HttpResponse
 
 def openvpn_password(request):
-    response_dict={}
-    r  = requests.get("http://www.vpnbook.com/freevpn")
-    data = r.text
-    soup = BeautifulSoup(data, "html.parser")
-    tags=soup.find_all('li')
-    for t in tags:
-        l=str(t.get_text())
-        if l.startswith("Password"):
-            now_password=l.split(" ",1)[-1]
-            response_dict['password']=now_password
+    try:
+        response_dict={}
+        r  = requests.get("http://www.vpnbook.com/freevpn")
+        data = r.text
+        soup = BeautifulSoup(data,'html.parser')
+        tags=soup.find_all('li')
+        for t in tags:
+            l=str(t.get_text())
+            if l.startswith("Password"):
+                now_password=l.split(" ",1)[-1]
+                response_dict['password']=now_password
+    except requests.exceptions.RequestException as e:
+        print e
     return HttpResponse(json.dumps(response_dict), content_type='application/javascript') 
 
