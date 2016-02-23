48542abc70300a319e3c09b6bf6c6b61b2726331
simplyom <omkumar.sahoo.ece14@iitbhu.ac.in>
2016-02-24 04:16:43 +0530

    implemented journey in predict cities,to be verified after caching

--- apis/views.py
+++ apis/views.py
@@ -358,7 +358,7 @@ def air_route(request):
     return HttpResponse(json.dumps(response_dict),content_type='application/javascript')
             
 
-def rail_route_to_airport(frm,to,date):
+def rail_route_to_airport(frm,to,date,travel_class):
     response_dict={} #example request=http://localhost:8000/airroute/?destination=Krishna%20Nagar,%20Mathura&departure_date=2016-04-03&travel_class=ECONOMY&origin=IIT%20Varanasi
     date=date[8:10]+'-'+date[5:7]+'-'+date[:4]
     print frm,to,date
@@ -394,7 +394,8 @@ def rail_route_to_airport(frm,to,date):
     except requests.exceptions.RequestException as e:
         print e
     print "second part done"
-    travel_class="3A"
+    if travel_class=="":
+        travel_class="3A"
     response_dict['from']=source_city
     response_dict['to']=dest_city
     try:
@@ -518,14 +519,14 @@ def air_route(request):
     if dic_from["distance"]>50:
         print "have to travel by train to airport"
         flag1=1
-        response_dict1=rail_route_to_airport(frm,dic_from["airport_name"],date)
+        response_dict1=rail_route_to_airport(frm,dic_from["airport_name"],date,"")
         print response_dict1
 
     if dic_to["distance"]>50:
         print "have to travel by train from airport"
         flag2=1
         print dic_to["airport_name"],to,date
-        response_dict2=rail_route_to_airport(dic_to["airport_name"],to,date)
+        response_dict2=rail_route_to_airport(dic_to["airport_name"],to,date,"")
         print response_dict2
     
 
@@ -573,6 +574,118 @@ def air_route(request):
         print e
     return HttpResponse(json.dumps(response_dict),content_type='application/javascript')
 
+
+def air_route_to_tour(frm,to,date,travel_class):
+    flag1,flag2=[0,0]
+    response_dict={} #example request=http://localhost:8000/airroute/?destination=Krishna%20Nagar,%20Mathura&departure_date=2016-04-03&travel_class=ECONOMY&origin=IIT%20Varanasi
+    if travel_class=="":
+        travel_class="ECONOMY"
+    print frm,to,date,travel_class
+    try:
+        payload={'address':frm,'key':google_geocoding_api_key}
+        frm_loc_ob = requests.get('https://maps.googleapis.com/maps/api/geocode/json',params=payload)
+        if frm_loc_ob.status_code == requests.codes.ok:
+            # print "OK"
+            frm_loc_json=frm_loc_ob.json()
+            # pp.pprint(frm_loc_json['results'][0]["geometry"]["location"])
+            location=frm_loc_json['results'][0]["geometry"]["location"]
+            frm_lat=location["lat"]
+            frm_lng=location["lng"]
+            try:
+                payload={"apikey":amadeus_api_key,'latitude':frm_lat,'longitude':frm_lng}
+                near_airport_loc_ob_1=requests.get('https://api.sandbox.amadeus.com/v1.2/airports/nearest-relevant',params=payload)
+                if near_airport_loc_ob_1.status_code == requests.codes.ok :
+                    near_airport_loc_json_1=near_airport_loc_ob_1.json()
+                    dic_from=near_airport_loc_json_1[0]
+            except requests.exceptions.RequestException as e:
+                print e
+    except requests.exceptions.RequestException as e:
+        print e
+    try:
+        payload={'address':to,'key':google_geocoding_api_key}
+        to_loc_ob = requests.get('https://maps.googleapis.com/maps/api/geocode/json',params=payload)
+        if to_loc_ob.status_code == requests.codes.ok:
+            # print "OK"
+            to_loc_json=to_loc_ob.json()
+            # pp.pprint(frm_loc_json['results'][0]["geometry"]["location"])
+            location=to_loc_json['results'][0]["geometry"]["location"]
+            to_lat=location["lat"]
+            to_lng=location["lng"]
+            try:
+                payload={"apikey":amadeus_api_key,'latitude':to_lat,'longitude':to_lng}
+                near_airport_loc_ob_2=requests.get('https://api.sandbox.amadeus.com/v1.2/airports/nearest-relevant',params=payload)
+                # print near_airport_loc_ob
+                if near_airport_loc_ob_2.status_code == requests.codes.ok:
+                    near_airport_loc_json_2=near_airport_loc_ob_2.json()
+                    dic_to=near_airport_loc_json_2[0]
+            except requests.exceptions.RequestException as e:
+                print e
+
+    except requests.exceptions.RequestException as e:
+        print e
+
+    if dic_from['airport']==dic_to['airport']:
+        return HttpResponse(json.dumps({'status':"False",'error':"no airroute possible"}),content_type='application/javascript')
+    if dic_from["distance"]>50:
+        print "have to travel by train to airport"
+        flag1=1
+        response_dict1=rail_route_to_airport(frm,dic_from["airport_name"],date,"")
+        print response_dict1
+
+    if dic_to["distance"]>50:
+        print "have to travel by train from airport"
+        flag2=1
+        print dic_to["airport_name"],to,date
+        response_dict2=rail_route_to_airport(dic_to["airport_name"],to,date,"")
+        print response_dict2
+    
+
+    try:
+        payload={                                         
+            'origin':dic_from['airport'],
+            'destination':dic_to["airport"],
+            'departure_date':date,
+            'apikey':amadeus_api_key,
+            'currency':"INR",
+            'number_of_results':"1",
+            "travel_class": travel_class}
+        print payload
+        flights_return_ob = requests.get('https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search',params=payload)
+        # print flights_return_ob.status_code
+        if flights_return_ob.status_code == requests.codes.ok:
+            flights_return_json=flights_return_ob.json()
+            flights_array=flights_return_json["results"]
+            response_dict['status']="True"
+            if flag1:
+                if response_dict1['not_found']:
+                    response_dict['step1']="From "+frm+" to "+dic_from['airport_name']+" travelling distance "+str(dic_from["distance"])+" km"    
+                else:    
+                    response_dict['step1.1']=response_dict1['step1']
+                    response_dict['step1.2']=response_dict1['step2']
+                    response_dict['step1.3']=response_dict1['step3']
+                    response_dict['train_to_airport_data']=response_dict1['train_data']
+            else:
+                response_dict['step1']="From "+frm+" to "+dic_from['airport_name']+" travelling distance "+str(dic_from["distance"])+" km"
+            response_dict['step2']="From "+dic_from['airport_name']+" to "+dic_to['airport_name']
+            response_dict['flights']=flights_array
+            if flag2:
+                if response_dict2['not_found']:
+                    response_dict['step3']="From "+dic_to['airport_name']+" to "+to+" travelling distance "+str(dic_to["distance"])+" km"
+                else:
+                    response_dict['step3.1']=response_dict2['step1']
+                    response_dict['step3.2']=response_dict2['step2']
+                    response_dict['step3.3']=response_dict2['step3']
+                    response_dict['train_from_airport_data']=response_dict2['train_data']
+            else:
+                response_dict['step3']="From "+dic_to['airport_name']+" to "+to+" travelling distance "+str(dic_to["distance"])+" km"
+        else:
+            return HttpResponse(json.dumps({'status':"False",'error':"departure_date should be after today's day"}),content_type='application/javascript')
+    except requests.exceptions.RequestException as e:
+        print e
+    return response_dict
+
+
+
 def valid_date(datestring):
     try:
         datetime.datetime.strptime(datestring, '%Y-%m-%d')
@@ -583,6 +696,80 @@ def valid_date(datestring):
 @ratelimit(key='ip', rate=rate, block=True)
 def predict_city(request):
     response_dict={} #example request=http://localhost:8000/predict_city/?departure_date=2016-04-03&origin=IIT%20Varanasi&duration=3
+    frm=request.GET.get('origin', '')
+    if frm=="":
+        return HttpResponse(json.dumps({'status':"False",'error':"plz specify the origin"}),content_type='application/javascript')
+    date=request.GET.get('departure_date', '')
+    if valid_date(date)==False:
+        if date!="":
+            return HttpResponse(json.dumps({'status':"False",'error':"plz enter date in yyyy-mm-dd format"}),content_type='application/javascript')
+        else:
+            return HttpResponse(json.dumps({'status':"False",'error':"plz specify the destination_date"}),content_type='application/javascript')
+    duration=request.GET.get('duration','')
+    max_min_ranges_rail={'1':{'min_range':0.00,'max_range':100.00},
+                      '2':{'min_range':100.00,'max_range':300.00},
+                      '3':{'min_range':200.00,'max_range':500.00},
+                      '4':{'min_range':400.00,'max_range':700.00},
+                      '5':{'min_range':500.00,'max_range':1000.00},
+                      '6':{'min_range':600.00,'max_range':1300.00},
+                      '7':{'min_range':700.00,'max_range':1500.00}}
+    max_distance=max_min_ranges_rail[duration]['max_range']
+    min_distance=max_min_ranges_rail[duration]['min_range']
+    try:
+        payload={'address':frm,'key':google_geocoding_api_key}
+        frm_loc_ob = requests.get('https://maps.googleapis.com/maps/api/geocode/json',params=payload)
+        if frm_loc_ob.status_code == requests.codes.ok:
+            # print "OK"
+            frm_loc_json=frm_loc_ob.json()
+            # print frm_loc_json
+            # pp.pprint(frm_loc_json['results'][0]["geometry"]["location"])
+            location=frm_loc_json['results'][0]["geometry"]["location"]
+            frm_lat=location["lat"]
+            frm_lng=location["lng"]
+            # print type(frm_lng)
+    except:
+        frm_lat=25.28   # of Varanasi in case of no internet #BHU #LOL
+        frm_lng=82.96
+    city_results=[]
+    count=0
+    for city in City.objects.all().order_by('-rating'):
+        dist=distance_on_unit_sphere(frm_lat,frm_lng,float(city.lat),float(city.lng))
+        if dist <= max_distance and dist >= min_distance:
+            # print city
+            count+=1
+            city_result={}
+            city_result['city']=city.name
+            city_places=city.places_set.all()
+            # print city_places
+            city_result_places=[]
+            for place in city_places:
+                p={}
+                p['name']=place.name
+                p['img']=place.img_src
+                p['about']=place.about
+                city_result_places.append(p)
+            city_result['city_places']=city_result_places
+            city_results.append(city_result)
+        if count >= 5:
+            break
+
+            # print city
+    response_dict['city_results']=city_results
+    return HttpResponse(json.dumps(response_dict),content_type='application/javascript')
+
+
+@ratelimit(key='ip', rate=rate, block=True)
+def predict_city_with_journey(request):
+    response_dict={} #example request=http://localhost:8000/predict_city/?departure_date=2016-04-03&origin=IIT%20Varanasi&duration=3
+    mode=request.GET.get('mode','')
+    if mode=="":
+        mode='rail'
+    travel_class=request.GET.get('travel_class','')
+    if travel_class=="":
+        if mode=='rail':
+            travel_class='3A'
+        else:
+            travel_class='ECONOMY'
     frm=request.GET.get('origin', '')
     if frm=="":
         return HttpResponse(json.dumps({'status':"False",'error':"plz specify the origin"}),content_type='application/javascript')
@@ -635,14 +822,26 @@ def predict_city(request):
                 p['img']=place.img_src
                 p['about']=place.about
                 city_result_places.append(p)
+            
+            try:
+                if mode=='rail':
+                    city_result['journey_details']=rail_route_to_airport(frm,city_result['city'],date,travel_class)
+                else:
+                    city_result['journey_details']=air_route_to_tour(frm,city_result['city'],date,travel_class)
+
+            except Exception as e:
+                city_result['journey_details']='We could not find any conventional route to '+city_result['city']+'. Soon we are going to launch our own special means! ;)'
             city_result['city_places']=city_result_places
             city_results.append(city_result)
-        if count >= 5:
+        if count >= 2:
             break
 
             # print city
     response_dict['city_results']=city_results
     return HttpResponse(json.dumps(response_dict),content_type='application/javascript')
+
+
+
 
 def distance_on_unit_sphere(lat1, long1, lat2, long2):
  
